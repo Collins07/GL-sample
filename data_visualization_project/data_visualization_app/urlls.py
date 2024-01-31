23795ce4c73636ca06269b3c350ab471bf00e6a1
Collins07 <collinsabaya07@gmail.com>
2024-01-31 21:07:09 +0300

    urls routing

---
+++ data_visualization_project/data_visualization_app/urlls.py
@@ -0,0 +1,10 @@
+# data_visualization_app/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add-environmental-data/', views.add_environmental_data, name='add_environmental_data'),
+    path('add-social-data/', views.add_social_data, name='add_social_data'),
+]
