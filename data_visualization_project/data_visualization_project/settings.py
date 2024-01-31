ebcae2dac313d91cb0080f3da08dafe1c7823659
Collins07 <collinsabaya07@gmail.com>
2024-01-31 21:14:06 +0300

    settings configurations

--- data_visualization_project/data_visualization_project/settings.py
+++ data_visualization_project/data_visualization_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data_visualization_app',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
