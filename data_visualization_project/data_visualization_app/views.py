4a8ac6723edbd8ff3a5c4abbb2daae6cd6d9934e
Collins07 <collinsabaya07@gmail.com>
2024-01-31 21:02:19 +0300

    views

--- data_visualization_project/data_visualization_app/views.py
+++ data_visualization_project/data_visualization_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# data_visualization_app/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import EnvironmentalData, SocialData
+from .forms import EnvironmentalDataForm, SocialDataForm
+
+def index(request):
+    environmental_data = EnvironmentalData.objects.all()
+    social_data = SocialData.objects.all()
+    return render(request, 'index.html', {'environmental_data': environmental_data, 'social_data': social_data})
+
+def add_environmental_data(request):
+    if request.method == 'POST':
+        form = EnvironmentalDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = EnvironmentalDataForm()
+    return render(request, 'form.html', {'form': form})
+
+def add_social_data(request):
+    if request.method == 'POST':
+        form = SocialDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = SocialDataForm()
+    return render(request, 'form.html', {'form': form})
